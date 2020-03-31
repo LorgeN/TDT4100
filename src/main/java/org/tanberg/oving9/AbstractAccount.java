9d9578948d943b1f97ea1c603df860055d913334
LorgeN <eirik@tanberg.org>
2020-03-31 19:37:56 +0200

    Abstract account real quick

---
+++ src/main/java/org/tanberg/oving9/AbstractAccount.java
@@ -0,0 +1,29 @@
+package org.tanberg.oving9;
+
+public abstract class AbstractAccount {
+
+    // Automatically defaults to 0
+    protected double balance;
+
+    public void deposit(double amount) throws IllegalArgumentException {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Provide a positive amount!");
+        }
+
+        this.balance += amount;
+    }
+
+    public void withdraw(double amount) throws IllegalArgumentException, IllegalStateException {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Provide a positive amount!");
+        }
+
+        this.internalWithdraw(amount);
+    }
+
+    abstract void internalWithdraw(double amount);
+
+    public double getBalance() {
+        return this.balance;
+    }
+}
